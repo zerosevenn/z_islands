df94bd18d58f9d03580905e5d456035e62973bf0
zero_sevenn <gscog05@gmail.com>
2024-05-28 03:37:02 -0300

    update

---
+++ src/main/java/me/zeroseven/island/GUI/IslandGUIHolder.java
@@ -0,0 +1,34 @@
+package me.zeroseven.island.GUI;
+
+import me.zeroseven.island.island.Island;
+import me.zeroseven.island.minions.Minion;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.jetbrains.annotations.NotNull;
+
+public class IslandGUIHolder implements InventoryHolder {
+
+    OfflinePlayer p;
+    Inventory inv;
+    Island island;
+
+    public IslandGUIHolder(OfflinePlayer p,  Island island) {
+        this.p = p;
+        this.island = island;
+    }
+
+    public OfflinePlayer getOfflinePlayer() {
+        return p;
+    }
+
+
+    public Island getIsland() {
+        return island;
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return inv;
+    }
+}
